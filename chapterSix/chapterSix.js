e42daf378ea9b387606e80a80fff901b0bc3d54f
Ryoung27 <RRYoung89@gmail.com>
2018-10-01 06:43:42 -0500

    finished with the book, wouldn't work through again.

---
+++ chapterSix/chapterSix.js
@@ -0,0 +1,18 @@
+//Chapter 6. Arrays
+
+let empty = [];
+let numbers = [
+    'zero', 'one', 'two', 'three', 'four',
+    'five', 'six', 'seven', 'eight', 'nine'
+];
+
+empty[1]
+numbers[1]
+
+empty.length
+numbers.length
+
+let i;
+for (i = 0; i < myArray.length; i += 1) {
+    document.writeIn(myArray[i]);
+}
